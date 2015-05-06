1dbe8d7cc49e1d322fc5667f1bc21f0a3e3ed397
bdz4sem <bdz4sem@gmail.com>
2015-05-06 20:18:03 +0400

    6 05
    ods

---
+++ OneDrive/Main.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace OneDrive
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node0");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.SignIn = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.meImage = new System.Windows.Forms.PictureBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.UploadButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SignOut = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button4 = new System.Windows.Forms.Button();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.OpenODSButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.meImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SignIn
+            // 
+            this.SignIn.Location = new System.Drawing.Point(525, 98);
+            this.SignIn.Name = "SignIn";
+            this.SignIn.Size = new System.Drawing.Size(80, 23);
+            this.SignIn.TabIndex = 0;
+            this.SignIn.Text = "Sign In";
+            this.SignIn.UseVisualStyleBackColor = true;
+            this.SignIn.Click += new System.EventHandler(this.SignInButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nameLabel.Location = new System.Drawing.Point(503, 95);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(0, 23);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "User";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode2.Name = "Drive";
+            treeNode2.Text = "Node0";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(367, 564);
+            this.treeView1.TabIndex = 6;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "fold2.jpg");
+            this.imageList1.Images.SetKeyName(1, "файл.png");
+            // 
+            // meImage
+            // 
+            this.meImage.Image = global::OneDrive.Properties.Resources.noavatar;
+            this.meImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("meImage.InitialImage")));
+            this.meImage.Location = new System.Drawing.Point(525, 12);
+            this.meImage.Name = "meImage";
+            this.meImage.Size = new System.Drawing.Size(80, 80);
+            this.meImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.meImage.TabIndex = 1;
+            this.meImage.TabStop = false;
+            this.meImage.WaitOnLoad = true;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(385, 12);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(75, 23);
+            this.DownloadButton.TabIndex = 8;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // UploadButton
+            // 
+            this.UploadButton.Location = new System.Drawing.Point(385, 41);
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(75, 23);
+            this.UploadButton.TabIndex = 9;
+            this.UploadButton.Text = "Upload";
+            this.UploadButton.UseVisualStyleBackColor = true;
+            this.UploadButton.Click += new System.EventHandler(this.UploadButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 582);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // SignOut
+            // 
+            this.SignOut.Location = new System.Drawing.Point(525, 127);
+            this.SignOut.Name = "SignOut";
+            this.SignOut.Size = new System.Drawing.Size(80, 23);
+            this.SignOut.TabIndex = 11;
+            this.SignOut.Text = "Sign Out";
+            this.SignOut.UseVisualStyleBackColor = true;
+            this.SignOut.Click += new System.EventHandler(this.SignOutButton_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(525, 156);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(80, 23);
+            this.webBrowser1.TabIndex = 12;
+            this.webBrowser1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(530, 553);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "CreateODS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(449, 527);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(156, 20);
+            this.passwordTextbox.TabIndex = 14;
+            // 
+            // OpenODSButton
+            // 
+            this.OpenODSButton.Location = new System.Drawing.Point(449, 553);
+            this.OpenODSButton.Name = "OpenODSButton";
+            this.OpenODSButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenODSButton.TabIndex = 15;
+            this.OpenODSButton.Text = "OpenODS";
+            this.OpenODSButton.UseVisualStyleBackColor = true;
+            this.OpenODSButton.Click += new System.EventHandler(this.OpenODSButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(617, 611);
+            this.Controls.Add(this.OpenODSButton);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.SignOut);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UploadButton);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.meImage);
+            this.Controls.Add(this.SignIn);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Mephi One Drive Client";
+            ((System.ComponentModel.ISupportInitialize)(this.meImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SignIn;
+        private System.Windows.Forms.PictureBox meImage;
+        public System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.Button UploadButton;
+        public System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SignOut;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Button OpenODSButton;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
